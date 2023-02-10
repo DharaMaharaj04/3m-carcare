30d167e49e21b7a8061e6c1713b6659f87a16a46
DHARA MAHARAJ <DharaMaharaj04>
2023-02-10 16:52:47 +0530

    format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 function Header() {
     return (
-            <header id="masthead" className=" ">
-                <div className="container-fuild">
-                    <nav className="navbar  navbar-light bacg-light">
-                        <a className="navbar-brand mt-2 mt-lg-0" href="#">
-                            <img src="/assets/img/3m-logo.jpg"  className="d-inline-block align-top" alt="logo" />
-                        </a>
-                        <div className="Social-media">
+        <header id="masthead" className=" ">
+            <div className="container-fuild">
+                <nav className="navbar  navbar-light bacg-light">
+                    <a className="navbar-brand mt-2 mt-lg-0" href="#">
+                        <img src="/assets/img/3m-logo.jpg"  className="d-inline-block align-top" alt="logo" />
+                    </a>
+                    <div className="Social-media">
                         <a href="https://www.facebook.com/3mcarcaresurat" className='soc-med'><i className="fa fa-facebook"></i></a>
                         <a href="https://www.instagram.com/3mcarcare.surat/" className='soc-med'><i className="fa fa-instagram"></i></a>
                         <a href="https://www.youtube.com/watch?v=zrB9AHE0kKY&t=5s" className='soc-med'><i className="fa fa-youtube"></i></a>
-                        </div>
-
-                    </nav>
-                </div>
-            </header>
+                    </div>
+                </nav>
+            </div>
+        </header>
             
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,16 +17,14 @@ export default class Home extends Component {
     };
 
 
-
     return (
         <div>
             <Helmet>
                 <title>3M car care surat | A Unit of Autohub Car services</title>
-            <meta name="description" content="3Mcarcare" />
-            <body id="page-home" />
-        </Helmet>
+                <meta name="description" content="3Mcarcare" />
+                <body id="page-home" />
+            </Helmet>
             <section className='car-image'>
-                
                 <OwlCarousel className="owl-theme" {...options} >
                     <div className='slide-1'>  
                         <div className='slide-img'>
@@ -47,7 +45,6 @@ export default class Home extends Component {
                         </div> 
                     </div>
                 </OwlCarousel>
-               
             </section>
             <section>
                 <div className='about container'>
@@ -59,108 +56,92 @@ export default class Home extends Component {
                 </div>
             </section>
             <article className="content">
-            <section className="content__descriptor">
-        <h2 className="content__title">About US</h2>
-      </section>
-      <section className="content__text-box">
-        <p className="content__text">
-        3M is fundamentally a science-based company producing the most imaginative products. Our success begins with our ability to apply our technologies - often in combination - to an endless array of customers car maintenance needs. The 3M Car Care Store is a place where technology, people and car care converge. This is where a car owner comes to appreciate the tremendous power of technology and 3M’s great belief in human connect While we pride ourselves in the science that goes into development of each of the products used at a 3M Car Car Store, it is also our endeavour that our customers understand what we have to offer in the smallest detail possible; This could be in the form of what products we use at these stores, what process we adopt or even the chemistry and the physical science behind what makes 3M products work so well So when you visit a 3M Car Care Store, you would meet a Store Manager who has soaked the 3M culture so well that sometimes he goes overboard in explaining what wonders 3M products can do on your car.
-        </p>
-        <p className="content__text">
-        Do excuse him if you find him too excited about his 3M Car Care Store, for they have been trained to talk with the customers about anything but selling! If you do happen to pick one of the several 3M car treatments, we bet you will, you can relax in the customer lounge and see your car getting transformed into a beauty. You have the option of reading, watching your favourite TV channel or working on your computer in a wi-fi enabled environment while you sip a cup of coffee. Of course…the Store Manager would be around if you wish to know more about “Keeping Your Car Looking New”.
-        </p>
-      </section>
-       </article> 
-    <section className='ser'>
-        <div className='container'>
-            <div className='row'>
-                <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M CERAMIC COATING</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M EXTERIOR COATING TREATMENT</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M FOAM WASH</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M ANTI CORROSSION TREATMENT</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M INTERIOR TREATMENT</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M NOMAD MATTING</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M SUNFILM</h3>
-          </div>
-        </div>
-    
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M PAINT PROTECTION FILM</h3>
-          </div>
-        </div>
-        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-                
-          <div className="card">
-            <div className="icon-wrapper">
-              <i className="fa fa-wrench"></i>
-            </div>
-            <h3>3M CAR CARE PRODUCTS</h3>
-          </div>
-        </div>
-            </div>
-        </div>
-    </section>
-
-               
+                <section className="content__descriptor">
+                    <h2 className="content__title">About US</h2>
+                </section>
+                <section className="content__text-box">
+                    <p className="content__text">3M is fundamentally a science-based company producing the most imaginative products. Our success begins with our ability to apply our technologies - often in combination - to an endless array of customers car maintenance needs. The 3M Car Care Store is a place where technology, people and car care converge. This is where a car owner comes to appreciate the tremendous power of technology and 3M’s great belief in human connect While we pride ourselves in the science that goes into development of each of the products used at a 3M Car Car Store, it is also our endeavour that our customers understand what we have to offer in the smallest detail possible; This could be in the form of what products we use at these stores, what process we adopt or even the chemistry and the physical science behind what makes 3M products work so well So when you visit a 3M Car Care Store, you would meet a Store Manager who has soaked the 3M culture so well that sometimes he goes overboard in explaining what wonders 3M products can do on your car.</p>
+                    <p className="content__text">Do excuse him if you find him too excited about his 3M Car Care Store, for they have been trained to talk with the customers about anything but selling! If you do happen to pick one of the several 3M car treatments, we bet you will, you can relax in the customer lounge and see your car getting transformed into a beauty. You have the option of reading, watching your favourite TV channel or working on your computer in a wi-fi enabled environment while you sip a cup of coffee. Of course…the Store Manager would be around if you wish to know more about “Keeping Your Car Looking New”.</p>
+                </section>
+            </article> 
+            <section className='ser'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M CERAMIC COATING</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>         
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M EXTERIOR COATING TREATMENT</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>        
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M FOAM WASH</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>     
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M ANTI CORROSSION TREATMENT</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>        
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M INTERIOR TREATMENT</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>       
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M NOMAD MATTING</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>            
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M SUNFILM</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>      
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M PAINT PROTECTION FILM</h3>
+                            </div>
+                        </div>
+                        <div className='column col-lg-4 col-md-4 col-sm-12 col-xs-12'>       
+                            <div className="card">
+                                <div className="icon-wrapper">
+                                    <i className="fa fa-wrench"></i>
+                                </div>
+                                <h3>3M CAR CARE PRODUCTS</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>       
       </div>
     )
   }
