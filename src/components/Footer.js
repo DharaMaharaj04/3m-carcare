c2290c91a8d784d282ff51a68d77c03c2734484b
DHARA MAHARAJ <DharaMaharaj04>
2023-02-10 12:59:25 +0530

    ini

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Footer() {
+
+    
+
+    return (
+        <div id="footer">
+        <footer className="footer-distributed">
+
+        <div className="footer-left">
+      
+          <h3>3M car care surat</h3>
+      
+      
+          <p className="footer-company-name">Scriptics.ai © 2023</p>
+        </div>
+      
+        <div className="footer-center">
+      
+          <div>
+            <i className="fa fa-map-marker"></i>
+            <p><span>Before Rajhans Theater Opp Radha Swami Temple Adajan</span>Hazira Rd, Pal, Surat, Gujarat 395009</p>
+          </div>
+      
+          <div>
+            <i className="fa fa-phone"></i>
+            <p>09924446456</p>
+            <i className=""></i>
+            <p>09725555544</p>
+          </div>
+      
+        </div>
+      
+        <div className="footer-right">
+      
+          <p  className="footer-company-about">
+            About the company <span></span>
+            <span>GST NO: 24ABFFA5567N2Z1</span>
+          </p>
+      
+      
+        </div>
+      
+      </footer>
+        </div>
+    )            
+}
+
+export default Footer
